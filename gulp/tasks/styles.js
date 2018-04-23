d733363ea623b11f039b5cd4ad96709baf485a53
woxxel <alexander.wollex.schmidt@googlemail.com>
2018-04-23 11:14:11 +0900

    up until responsive images

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+    autoprefixer = require('autoprefixer'),   // allows automatic prefixing
+    postcss= require('gulp-postcss'),
+    cssvars = require('postcss-simple-vars'), // allows variables in css
+    nested = require('postcss-nested'),       // allows for nested css code
+    cssImport = require('postcss-import'),    // enable inline-importing of css files
+    mixins = require('postcss-mixins');
+
+
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+          .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+          .on('error', function(errorInfo){
+            console.log(errorInfo.toString());
+            this.emit('end');
+          })
+          .pipe(gulp.dest('./app/temp/styles'));
+});
