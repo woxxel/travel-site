33832fad97002f9815b20e8f3760f8586a8f1a63
woxxel <alexander.wollex.schmidt@googlemail.com>
2018-10-11 19:59:37 +0900

    mal wieder commiten...

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,6 +5,7 @@ var gulp = require('gulp'),
 gulp.task('watch',function(){
   
   browserSync.init({
+//     browser: "google chrome",
     notify:false,
     server: {
       baseDir: "app"
@@ -18,9 +19,18 @@ gulp.task('watch',function(){
   watch('./app/assets/styles/**/*.css',function() {
     gulp.start('cssInject');
   });
+  
+  watch('./app/assets/scripts/**/*.js', function() {
+    gulp.start('scriptsRefresh');
+  })
+  
 });
 
 gulp.task('cssInject', ['styles'], function() {
   return gulp.src('./app/temp/styles/styles.css')
     .pipe(browserSync.stream());
 });
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+  browserSync.reload();
+})
